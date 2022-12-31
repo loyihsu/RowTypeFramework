c04ffa6a3c72134359bc3c2034697a535db6b952
Yu-Sung Loyi Hsu <loyihsu@icloud.com>
2022-12-31 10:29:31 +0800

    feat: update protocol signature to use type erased row type

--- Sources/RowTypeTableView/UITableViewRowCell.swift
+++ Sources/RowTypeTableView/UITableViewRowCell.swift
@@ -10,5 +10,17 @@ import UIKit
 
 /// The common row cell protocol for all `UITableView` row cells. All row cells need to have a model defined for the row, which will be passed from row to model as is provided in the `RowType` model protocol method.
 public protocol UITableViewRowCell: UITableViewCell {
-    var model: RowModelType! { get set }
+    var model: AnyRowModelType! { get set }
+}
+
+public struct AnyRowModelType {
+    var wrapped: any RowModelType
+
+    public func casted<T>(to _: T.Type) -> T? {
+        return wrapped as? T
+    }
+
+    public func forced<T>(to type: T.Type) -> T {
+        return casted(to: type)!
+    }
 }
